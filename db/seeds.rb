50a05dd5d341d88dad7a13cd40753d520e45ffb1
Marior9 <mariorodriguezan@gmail.com>
2019-04-29 12:14:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.find_or_create_by(title: "New Post", description: "New Description")
+Post.find_or_create_by(title: "Second Post", description: "Second new description")
+Post.find_or_create_by(title: "Thrid Post", description: "Thrid new description")
